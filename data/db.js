46cd29f4b69fa8e5e3110c40d17285fd420159db
Mario Cesena <112787277+mariocc22@users.noreply.github.com>
2023-09-23 09:15:31 -0700

    feat: adding dummy data for the reports

--- data/db.js
+++ data/db.js
@@ -1,17 +1,74 @@
-// dummy data
 {
-  tasks: [
+  const reports = [
     {
-      id: 1,
-      text: "Doctors Appointment",
-      day: "Feb 5th at 2:30pm",
-      reminder: true,
+      type: "construction",
+      address: "5678 Elm St",
+      endDate: "2022-12-31",
+      image: require("../src/assets/img_1.jpg"),
+      reporterName: "John Doe",
     },
     {
-      id: 2,
-      text: "Meeting at School",
-      day: "Feb 6th at 1:30pm",
-      reminder: true,
+      type: "outOfOrder",
+      address: "2344 Elm St",
+      endDate: "2022-11-15",
+      image: require("../src/assets/img_2.jpg"),
+      reporterName: "Jane Smith",
+    },
+    {
+      type: "construction",
+      address: "9876 Oak Ave",
+      endDate: "2022-10-20",
+      image: require("../src/assets/img_3.jpg"),
+      reporterName: "David Johnson",
+    },
+    {
+      type: "outOfOrder",
+      address: "9876 Oak Ave",
+      endDate: "2022-09-05",
+      image: require("../src/assets/img_4.jpg"),
+      reporterName: "Emily Brown",
+    },
+    {
+      type: "construction",
+      address: "321 Elmwood Dr",
+      endDate: "2022-08-14",
+      image: require("../src/assets/img_5.jpg"),
+      reporterName: "Michael Lee",
+    },
+    {
+      type: "outOfOrder",
+      address: "321 Elmwood Dr",
+      endDate: "2022-07-23",
+      image: require("../src/assets/img_1.jpg"),
+      reporterName: "Sophia Wilson",
+    },
+    {
+      type: "construction",
+      address: "654 Pine Lane",
+      endDate: "2022-06-10",
+      image: require("../src/assets/img_2.jpg"),
+      reporterName: "Daniel Garcia",
+    },
+    {
+      type: "outOfOrder",
+      address: "654 Pine Lane",
+      endDate: "2022-05-09",
+      image: require("../src/assets/img_3.jpg"),
+      reporterName: "Olivia Martinez",
+    },
+    {
+      type: "construction",
+      address: "789 Cedar Road",
+      endDate: "2022-04-22",
+      image: require("../src/assets/img_4.jpg"),
+      reporterName: "William Anderson",
+    },
+    {
+      type: "outOfOrder",
+      address: "789 Cedar Road",
+      endDate: "2022-03-30",
+      image: require("../src/assets/img_5.jpg"),
+      reporterName: "Sophie Thomas",
     },
   ];
 }
