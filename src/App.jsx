a0f9e9ff32ecb1fc97265779b3b5ea86aa0c1ccf
Tony-Thawatchai <au.thawatchai@gmail.com>
2023-09-23 10:34:27 -0700

    add select view button

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import HomeLayout from './components/HomeLayout'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import HomeLayout from "./components/HomeLayout";
+import ListLayout from "./components/ListLayout";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [selectView, setSelectView] = useState("mapView");
+  console.log(selectView);
   return (
     <>
-     <HomeLayout  />
+      <div style={{ display: "flex", gap: ".5rem", marginBottom: "" }}>
+        <button value={"mapView"} onClick={()=> {setSelectView("mapView")}}>mapView</button>
+        <button value={"listView" } onClick={()=> {setSelectView("listView")}}>listView</button>
+      </div>
+
+      {selectView == "mapView" && <HomeLayout />}
+      {selectView == "listView" && <ListLayout /> }
+      {/* <HomeLayout /> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
