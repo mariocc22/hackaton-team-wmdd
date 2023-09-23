19766722888450c2a2286e95c1858e0f1bdb9238
HT <rhizodont@gmail.com>
2023-09-23 15:41:14 -0700

    style: listView / search

--- src/components/ListLayout.jsx
+++ src/components/ListLayout.jsx
@@ -1,4 +1,7 @@
 import { useEffect, useState } from "react";
+import { IconThumbUp, IconThumbDown, IconSlash } from '@tabler/icons-react';
+
+import "./ListLayout.css"
 
 const ListLayout = ({ data }) => {
   const [city, setCity] = useState("");
@@ -20,45 +23,167 @@ const ListLayout = ({ data }) => {
     setFilterData(fetchData);
   };
 
-  return (
+  const searchSubmit = (e) => {
+    e.preventDefault();
+  }
 
+  const [popup, setPopup] = useState("")
+
+  const upvote = (e) => {
+    e.preventDefault();
+    setPopup(!popup);
+
+    let parent = e.target.closest("[data-key]");
+    let thumbup = parent.querySelector(".tabler-icon-thumb-up");
+    let thumbdown = parent.querySelector(".tabler-icon-thumb-down");
+
+    if(thumbup.getAttribute("fill") === "none"){
+      thumbup.setAttribute("fill","var(--accent-green")
+    } else {
+      thumbup.setAttribute("fill","none")
+    }
+
+    if(thumbdown.getAttribute("fill") !== "none"){
+      thumbdown.setAttribute("fill","none")
+      thumbup.setAttribute("fill","var(--accent-green")
+    }
+
+    setPopup(!popup)
+  }
+
+  const downvote = (e) => {
+    e.preventDefault();
+    setPopup(!popup);
+
+    let parent = e.target.closest("[data-key]");
+    let thumbup = parent.querySelector(".tabler-icon-thumb-up");
+    let thumbdown = parent.querySelector(".tabler-icon-thumb-down");
+    
+    if(thumbdown.getAttribute("fill") === "none"){
+      thumbdown.setAttribute("fill","var(--accent-red")
+    } else {
+      thumbdown.setAttribute("fill","none")
+    }
+
+    if(thumbup.getAttribute("fill") !== "none"){
+      thumbup.setAttribute("fill","none")
+      thumbdown.setAttribute("fill","var(--accent-red")
+    }
+
+    setPopup(!popup)
+  }
+
+  const buttonOK = (e) => {
+    e.preventDefault();
+    setPopup(!popup)
+  }
+
+  return (
+    <>
     <div className="reportModel" style={{ height: "90vh", width: "90vw" }}>
-      <div className="searchField">
-        <input
-          text="type"
-          placeholder="search by city"
-          value={city}
-          onChange={getCity}
-        ></input>
-      </div>
-      <div className="showData">
-        <ul>
-            {filterData.length==0?(data.map((item, index) => (
-            <div className="data" key={index}>
-              <li>
-                <p>{item.type} </p>
-                <p>{item.address}</p>
-              </li>
-            </div>
-          ))):(filterData.map((item, index) => (
-            <div className="data" key={index}>
-              <li>
-                <p>{item.type} </p>
-                <p>{item.address}</p>
-              </li>
-            </div>
-          )))}
-          {/* {filterData.map((item, index) => (
-            <div className="data" key={index}>
-              <li>
-                <p>{item.type} </p>
-                <p>{item.address}</p>
-              </li>
-            </div>
-          ))} */}
-        </ul>
-      </div>
+      <div className="searchcont">
+        <form className="searchField" onSubmit={searchSubmit}>
+          <input
+            type="text"
+            placeholder="Search by city..."
+            value={city}
+            onChange={getCity}
+          ></input>
+          <div className="btnh">
+            <button type="submit">Search</button>
+          </div>
+        </form>{/* end searchField */}
+        <div className="showData">
+          <ul>
+              {filterData.length==0?(data.map((item, index) => (
+                <li className="data" key={index} data-key={item.id}>
+                  <div className="data_flex">
+                    <div className="data_text">
+                    <div className="data_row">
+                        <b>Place:</b>
+                        <span> {item.name}</span>
+                      </div>
+
+                      <div className="data_row">
+                        <b>Address:</b>
+                        <span> {item.address}</span>
+                      </div>
+
+                      <div className="data_row">
+                        <b>Problem Type:</b>
+                        <span> {
+                          item.type === "outOfOrder" ? "Out of order" :
+                          item.type === "temporaryClose" ? "Temporarily Closed" :
+                          item.type === "construction" ? "Construction" :
+                          "Other"
+                        }</span>
+                      </div>
+
+                      <div className="data_row">
+                        <img src={item.image} alt={"image"}/>
+                      </div>
+
+                      <div className="data_row">
+                        <b>Reported by:</b>
+                        <span> {item.reporterName}</span>
+                      </div>
+                      
+                    </div>
+
+                    <div className="right">
+                      <p>Did you find<br/>this helpful?</p>
+                      
+                      <div className="thumbs">
+                        <IconThumbUp onClick={upvote} size={22} color="var(--text)" stroke={2}/>
+                        <IconSlash size={22} color="var(--text)" stroke={2}/>
+                        <IconThumbDown onClick={downvote} size={22} color="var(--text)" stroke={2}/>
+                      </div>
+                      
+                    </div>
+                  </div>
+                </li>
+                
+            ))):(filterData.map((item, index) => (
+                <li className="data" key={index}>
+                  <div className="data_row">
+                    <span> {item.type}</span>
+                  </div>
+
+                  <div className="data_row">
+                    <span> {
+                      item.type === "outOfOrder" ? "Out of order" :
+                      item.type === "temporaryClose" ? "Temporarily Closed" :
+                      "Default Value"
+                    }</span>
+                  </div>
+                </li>
+            )))}
+            {/* {filterData.map((item, index) => (
+              <div className="data" key={index}>
+                <li>
+                  <p>{item.type} </p>
+                  <p>{item.address}</p>
+                </li>
+              </div>
+            ))} */}
+          </ul>
+        </div> {/* end showData */}
+      </div>{/* end searchcont */}
     </div>
+
+    { popup ? 
+        <div id="ok-popup">
+            <div className="popup-box">
+                Thank you for your feedback!
+
+                <div className="btns">
+                    <button onClick={buttonOK}>Okay</button>
+                </div>
+                
+            </div>
+        </div>
+    : ""}
+    </>
   );
 };
 
