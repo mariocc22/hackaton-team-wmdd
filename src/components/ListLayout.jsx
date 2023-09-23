58f81071d81fe6fe8bc327f8d382f9008b25f03d
Mahima Mukhi <116036967+mmukhi00@users.noreply.github.com>
2023-09-23 11:59:55 -0700

    create report list view

--- src/components/ListLayout.jsx
+++ src/components/ListLayout.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-function ListLayout() {
+const ListLayout = ({data}) => {
+    const [city, setCity] = useState('')
+    const [filterData, setFilterData]=useState([{}])
+    const getCity = (event) => {
+        setCity(event.target.value)
+        console.log(city)
+        // console.log(data)
+        // getFilter()
+    }
+
+    useEffect(() => {
+        getFilter()
+    },[city])
+    
+    const getFilter = () =>
+    { 
+        console.log(city)
+        const fetchData = data.filter(item => item.city == city)
+        setFilterData(fetchData)
+        console.log(fetchData)
+
+    }
   return (
-    <div style={{height:"90vh", width:"90vw"}}>
-      This will be list of all report and filter
-    </div>
+      <div className='reportModel' style={{height:"90vh", width:"90vw"}}>
+          <div className='searchField'>
+          <input text="type" placeholder='search by city' value={city} onChange={getCity}></input>
+          </div>
+          <div className='showData'>
+               <ul>
+              {filterData.map((item) => (
+              <div className='data'>
+                      <li>
+                          <p>{item.type} </p>
+                          <p>{item.address}</p>
+                      </li> 
+             </div>
+          ))}
+        </ul>
+          </div>
+          
+      </div>
   )
 }
 
