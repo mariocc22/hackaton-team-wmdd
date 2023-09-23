9c6f9526dd610cffb92d10c34222003132e5a883
Mario Cesena <112787277+mariocc22@users.noreply.github.com>
2023-09-23 12:35:19 -0700

    feat: adding a modal component to open report

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
-
-
 import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
-
 // import layout components
-import HomeLayout from './components/HomeLayout'
+import HomeLayout from "./components/HomeLayout";
 // fetch tasks from the server
 import { fetchReports } from "../utils/helpers";
-
 
 function App() {
   const [count, setCount] = useState(0);
@@ -24,11 +20,11 @@ function App() {
     fetchData();
   }, []);
 
+  console.log(reports);
+
   return (
     <>
-
-     <HomeLayout  />
-
+      <HomeLayout />
     </>
   );
 }
--- src/components/HomeLayout.jsx
+++ src/components/HomeLayout.jsx
@@ -1,18 +1,18 @@
-import React from "react";
-
-import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
+import { GoogleMap, useLoadScript } from "@react-google-maps/api";
 import { useMemo } from "react";
 import "../../src/App.css";
+import OpenReportModal from "./OpenReportModal";
 
 function HomeLayout() {
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: "AIzaSyAafj5ZO6dNK-OloRNXjot-Jk4TEgdKZLo",
   });
-  const center = useMemo(() => ({ lat: 49.2827, lng: -123.1207}), []);
+  const center = useMemo(() => ({ lat: 49.2827, lng: -123.1207 }), []);
 
   return (
     <>
       <h1>Home</h1>
+      <OpenReportModal />
       <div className="mapWrapperStyle">
         {!isLoaded ? (
           <h1>Loading...</h1>
@@ -21,6 +21,9 @@ function HomeLayout() {
             mapContainerClassName="map-Container"
             center={center}
             zoom={13}
+            onClick={(e) => {
+              console.log("this is the info!", e);
+            }}
           />
         )}
       </div>
