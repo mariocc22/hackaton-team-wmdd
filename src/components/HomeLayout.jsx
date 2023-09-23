d05ef01807aa74d0b83dc030265e8b61699ff791
Tony-Thawatchai <au.thawatchai@gmail.com>
2023-09-23 13:12:06 -0700

    1312

--- src/components/HomeLayout.jsx
+++ src/components/HomeLayout.jsx
@@ -29,7 +29,7 @@ console.log(data)
 
                 {
                     data.map((item,index) => (
-                        <Marker key={index} position={{ lat: item.lat, lng: item.lng }} />
+                        <Marker key={index} position={{ lat: item.lat, lng: item.lng }} onClick={()=>{console.log(item.address)}} />
                     ))
                 }
               {/* <Marker position={center} /> */}
